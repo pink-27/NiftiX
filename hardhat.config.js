57e9ad40a3747df7e21a9fb80b4fbcba87cc434d
pink-27 <98738847+pink-27@users.noreply.github.com>
2023-04-07 20:29:21 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,36 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+	const accounts = await hre.ethers.getSigners();
+
+	for (const account of accounts) {
+		console.log(account.address);
+	}
+});
+
+module.exports = {
+	defaultNetwork: "hardhat",
+	networks: {
+		hardhat: {
+			chainId: 1337,
+		},
+		goerli: {
+			url: "https://eth-goerli.g.alchemy.com/v2/qsENwDh_-A6ds1FnycjIab0jj8UYkVeH",
+			accounts: [
+				"d87a73f4ab45cc0e872bb7f0af470039d99e56a93892a13cb397e75c08c4a94a",
+			],
+		},
+	},
+	solidity: {
+		version: "0.8.4",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 200,
+			},
+		},
+	},
+};
